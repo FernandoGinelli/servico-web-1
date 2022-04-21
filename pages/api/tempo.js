caf0f2e6b73bb78b413c96730e7e3f54324eacf0
unknown <fernandoginelli@gmail.com>
2022-04-21 17:27:27 -0300

    aprendendo Next.js - teste de chaves api vercel.app

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
-function tempo(request, response) {
+async function tempo(request, response) {
     const dynamicDate = new Date();
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+
+    const subscribersResponse = await fetch(`${apiSecret}/api/tempo`)
+    const subscribersResponseJson = await subscribersResponse.json()
+    const inscritos = subscribersResponseJson.date;
 
     response.json({
-        date: dynamicDate.toGMTString()  
+        date: inscritos,  
+        inscritos: inscritos
     })
 }
 
