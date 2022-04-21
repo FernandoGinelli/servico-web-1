7a482b4d4312d94f178cf94dd3c7241c1a9a6fc8
unknown <fernandoginelli@gmail.com>
2022-04-21 18:24:19 -0300

    aprendendo Next.js - correção de erros

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -5,7 +5,8 @@ async function tempo(request, response) {
    //const subscribersResponse = await fetch(`http://localhost:3000/tempo`)
     //const subscribersResponseJson = await subscribersResponse.json()
     //const inscritos = subscribersResponseJson.date;
-
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
+    
     response.json({
         date: dynamicDate,  
         
